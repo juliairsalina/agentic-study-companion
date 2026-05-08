9dcad653950d6118b910cd21e9cc123e0783385d
Julia <h1itsjulls@gmail.com>
2026-05-08 19:47:12 +0900

    fix: update CORS settings to allow all origins and revert backend base URL to local development

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,11 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[  
-        "http://localhost:5500", 
-        "http://127.0.0.1:5500",
-        "https://ashy-field-0d5adff00.7.azurestaticapps.net",
-        ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
