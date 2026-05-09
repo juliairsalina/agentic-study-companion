8f73510c548a9c3111549a865b21bfdca9faaf61
Julia <h1itsjulls@gmail.com>
2026-05-09 21:58:06 +0900

    Update CORS middleware settings and change backend base URL in app config

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://yellow-moss-0a08bda1e.7.azurestaticapps.net",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
